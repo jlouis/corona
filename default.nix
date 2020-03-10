8f068f868b4a7d5c510fb348ec852d2db3692846
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2020-03-10 22:41:19 +0100

    Nix for easier R management

---
+++ default.nix
@@ -0,0 +1,20 @@
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+in with pkgs; {
+  myProject = stdenv.mkDerivation {
+    name = "myProject";
+    version = "1";
+    src = if pkgs.lib.inNixShell then null else nix;
+    
+    buildInputs = with rPackages; [
+      R
+      curl
+      libxml2
+      openssl
+      tidyverse
+      jsonlite
+    ];
+  };
+}
+
