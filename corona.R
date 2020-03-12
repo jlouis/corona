1613b8f9f34ba2a26fb733fa6ddd38916b9ff695
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2020-03-12 11:38:25 +0100

    Add some simple core processing and plotting

--- corona.R
+++ corona.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(lubridate)
 library(fs)
 
 # Where to grab from the COVID-19 data set.
@@ -8,13 +9,23 @@ data_dir <- "../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports"
 csv_files <- fs::dir_ls(data_dir, regexp = "\\.csv$")
 
 # Raw data. Read in those CSV files, mutate each line to contain a date based on the file name.
+# While here, also fix variable names so they are a bit easier to work with
 raw <- csv_files %>% set_names() %>%
-  map_dfr(read_csv, .id = "source") %>%
-  mutate(source = basename(source)) %>%
-  mutate(source = as.POSIXct(gsub('.*([0-9]{4}-[0-9]{2}-[0-9]{2})+.*','\\1',source), format='%m-%d-%y', tz='UTC'))
+  map_dfr(read_csv, .id = "date") %>%
+  mutate(date = basename(date)) %>%
+  mutate(date = as.POSIXct(gsub('.*([0-9]{4}-[0-9]{2}-[0-9]{2})+.*','\\1',date), format='%m-%d-%y', tz='UTC')) %>%
+  rename(province = `Province/State`, country = `Country/Region`, last_update = `Last Update`,
+    confirmed = `Confirmed`, deaths = `Deaths`, recovered = `Recovered`, lat = `Latitude`, lng = `Longitude`)
 
-# Next step, tidy up the data set. There is a concept of tidy data, which is the kind of data
-# one should strive to have. Thus, use the raw data set as a base for creation of the tidy data set
+# Note the data is already tidy. So we don't have to do anything ekstra with the data.
+dk <- raw %>% filter(country == "Denmark") %>% filter(is.na(province) | province == "Denmark")
+
+p <- ggplot(dk, aes(x=as_date(date)-today(), y=confirmed))
+p + geom_point() +
+    geom_smooth(method="loess", formula = (y ~ x)) +
+    scale_y_log10()
+
+ggsave("dk.pdf", height=5, width=8)
 
 
 
